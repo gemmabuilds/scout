7619523a04e7ab19dea183a2a1ce663ec7cb8f98
Logan Fitzgibbons <logiwan992@gmail.com>
2017-03-13 14:03:10 -0500

    Functionality for adding players to a game

--- client/pages/game.js
+++ client/pages/game.js
@@ -2,12 +2,27 @@ import { Games } from '../../collections/games.js';
 
 Template.game.helpers({
   game: function() {
-    return Games.findOne({short_id: Router.current().params._id});
+    return Games.findOne({short_id: Router.current().params.short_id});
   },
 });
 
+Template.game.events({
+  'submit #join-game'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const playerName = target.playerName.value;
+
+    var game = Games.findOne({short_id: Router.current().params.short_id});
+
+    Games.update({ _id: game._id }, {
+      $push: { players: { name: playerName, hand: [] } },
+    });
+  },
+})
+
 Template.gameTable.helpers({
   game: function() {
-    return Games.findOne({short_id: Router.current().params._id});
+    return Games.findOne({short_id: Router.current().params.short_id});
   },
 });
