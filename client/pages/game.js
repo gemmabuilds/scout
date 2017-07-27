671650441593ee248eb351ce3c25edf80c222255
Logan Fitzgibbons <logiwan992@gmail.com>
2017-07-27 18:38:55 -0500

    continued work on game pages

--- client/pages/game.js
+++ client/pages/game.js
@@ -1,8 +1,33 @@
 import { Games } from '../../collections/games.js';
 
-Template.game.helpers({
+Template.enterGame.created = function () {
+  // here `this` refers to template instance
+  this.game = -1;
+};
+
+Template.enterGame.helpers({
   game: function() {
-    return Games.findOne({short_id: Router.current().params._id});
+    var g = Games.findOne({short_id: Router.current().params._id});
+    Template.instance().game = g;
+    return g;
+  },
+});
+
+Template.enterGame.events({
+  'submit #enter-game'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const playerName = target.playerName.value;
+
+    var player = {
+      name: playerName,
+      hand: [],
+    };
+
+    Games.update(Template.instance().game._id, {
+      $push: { players: player }
+    });
   },
 });
 
